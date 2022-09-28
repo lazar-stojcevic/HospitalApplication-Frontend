9c33cfeb7511fa7c299b471ebaecbcb03df5238e
lazar.stojcevic <stojcevic00@gmail.com>
2022-09-28 22:29:50 +0200

    Enable doctor to change his own data

--- HospitalApplicationFront/src/app/profiles/doctor-profile/doctor-profile.component.ts
+++ HospitalApplicationFront/src/app/profiles/doctor-profile/doctor-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormControl, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { DoctorService } from 'src/app/services/doctor.service';
 
@@ -9,6 +10,17 @@ import { DoctorService } from 'src/app/services/doctor.service';
 })
 export class DoctorProfileComponent implements OnInit {
   doctor: any;
+  routeId = this.route.snapshot.paramMap.get('id');
+  userId = localStorage.getItem('id');
+
+  usernameForm = new FormControl('', [Validators.required, Validators.maxLength(30), Validators.pattern(/^[a-z0-9]+$/i)]);
+  firstNameForm = new FormControl('', [Validators.required, Validators.maxLength(30), Validators.pattern(/^[a-z0-9]+$/i)]);
+  surnameForm = new FormControl('', [Validators.required, Validators.maxLength(30), Validators.pattern(/^[a-z0-9]+$/i)]);
+  personalNumberForm = new FormControl('', [Validators.required, Validators.pattern(/^[0-9]{13}/i)]);
+  phoneNumberForm = new FormControl('', [this.patternValidator('[- +()0-9]+', { phoneNumber: true })]);
+  emailForm = new FormControl('', [Validators.required, Validators.email]);
+  dateOfBirthForm = new FormControl('', [Validators.required]);
+  medicalSpecialityForm = new FormControl('', [Validators.required]);
 
   constructor(
     private route: ActivatedRoute,
@@ -26,12 +38,37 @@ export class DoctorProfileComponent implements OnInit {
     }
 
   ngOnInit(): void {
-    const id = this.route.snapshot.paramMap.get('id');
-    if (id != null){
-      this.doctorService.getOneDoctors(id).subscribe(res => {
+    if (this.routeId != null){
+      this.doctorService.getOneDoctors(this.routeId).subscribe(res => {
         this.doctor = res;
       })
     }
+  }
+
+  saveChanges(){
+    this.doctor.dateOfBirth.setDate(this.doctor.dateOfBirth.getDate() + 1);
+    this.doctorService.updateDoctorData(
+      this.doctor.id,
+      this.doctor.username, 
+      this.doctor.firstName, 
+      this.doctor.surname,
+      this.doctor.personalNumber,
+      this.doctor.phoneNumber,
+      this.doctor.email,
+      this.doctor.dateOfBirth,
+      this.doctor.medicalSpeciality).subscribe(res => {
+        alert('Podaci su sačuvani');
+      })
+  }
+
+  private patternValidator(regex: string, error: ValidationErrors): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } => {
+      if (!control.value) {
+        return null as any;
+      }
+      const valid = RegExp(regex).test(control.value);
+      return valid ? null as any : error;
+    };
   }
 
 }
--- HospitalApplicationFront/src/app/services/doctor.service.ts
+++ HospitalApplicationFront/src/app/services/doctor.service.ts
@@ -10,11 +10,34 @@ export class DoctorService {
 
   constructor(private http: HttpClient, private router: Router) { }
 
-  getAllDoctors(){
+  getAllDoctors() {
     return this.http.get(environment.serverUrl + 'doctors');
   }
 
-  getOneDoctors(id: string){
+  getOneDoctors(id: string) {
     return this.http.get(environment.serverUrl + 'doctors/' + id);
+  }
+
+  updateDoctorData(
+    id: string,
+    username: string,
+    firstname: string,
+    surname: string,
+    personalNumber: string,
+    phoneNumber: string,
+    email: string,
+    dateOfBirth: Date,
+    medicalSpeciality: string) {
+    return this.http.put(environment.serverUrl + 'doctors/' + id, {
+      id: id,
+      username: username,
+      firstName: firstname,
+      surname: surname,
+      personalNumber: personalNumber,
+      phoneNumber: phoneNumber,
+      email: email,
+      dateOfBirth: dateOfBirth,
+      medicalSpeciality: medicalSpeciality
+    });
   }
 }
